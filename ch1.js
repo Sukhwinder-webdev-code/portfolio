fa72c2e3d18f06a3617fe225fddef062e8b32150
Sukhwinder Kaur <sukhi.handa43@gmail.com>
2025-11-01 13:46:45 +0000

    fall files added

---
+++ ch1.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("read-more");
+const more = document.getElementById("more");
+if (btn && more) {
+
+    btn.addEventListener('click', () => {
+        if (more.style.display === "none") {
+            more.style.display = "inline";
+            btn.textContent = "Read Less";
+        }
+        else {
+            more.style.display = "none";
+            btn.textContent = "Read More";
+        }
+    });
+}
+const hamMenu = document.querySelector("i.ham-menu");
+const navLinks = document.querySelector("nav ul");
+if (hamMenu && navLinks) {
+    hamMenu.addEventListener('click', () => {
+        navLinks.classList.toggle("show");
+    });
+}
